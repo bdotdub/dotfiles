ac4cae31242a2348a7cae34f02458dffc0d2c629
Benny Wong <bdotdub@gmail.com>
2009-10-31 14:11:40 -0400

    adding install and uninstall scripts

---
+++ install.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'ftools'
+
+repo_dir      = File.dirname(__FILE__)
+home_dir      = ENV['HOME']
+config_backup = File.join home_dir, '.dotfiles'
+
+# Move into this dir
+Dir.chdir repo_dir
+
+ignore= ['install.rb', 'uninstall.rb', 'README.md', '.git', '.gitignore']
+Dir.glob('*').reject{|file| ignore.include?(file) }.each do |config_file|
+  dot_config_file           = ".#{config_file}"
+  dot_config_file_full_path = File.join home_dir, dot_config_file
+  config_file_full_path     = File.join repo_dir, config_file
+
+  if File.exists?(File.join(home_dir, dot_config_file))
+    Dir.mkdir config_backup unless File.exists?(config_backup)
+
+    puts "Moving file from #{dot_config_file_full_path} to #{File.join(config_backup, config_file)}"
+    File.move dot_config_file_full_path, File.join(config_backup, dot_config_file)
+  end
+
+  puts "Linking file #{config_file_full_path} to #{dot_config_file_full_path}"
+  File.symlink config_file_full_path, dot_config_file_full_path
+end
+
---
+++ uninstall.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'ftools'
+
+home_dir      = ENV['HOME']
+config_backup = File.join home_dir, '.dotfiles'
+
+Dir.chdir home_dir
+Dir.glob(File.join(config_backup, '.*')).each do |dot_config_file|
+  config_file               = File.basename dot_config_file
+  dot_config_file_full_path = File.join home_dir, config_file
+  backup_file_full_path     = dot_config_file
+
+  next if config_file =~ /^\.+$/
+  next if config_file =~ /dotfiles/
+
+  if File.exists?(dot_config_file_full_path)
+    puts "Removing current config file: #{dot_config_file_full_path}"
+    File.safe_unlink(dot_config_file_full_path)
+  end
+
+  if File.symlink?(backup_file_full_path)
+    backup_file_full_path = File.readlink backup_file_full_path
+  end
+  puts "Moving backup file: #{backup_file_full_path} => #{dot_config_file_full_path}"
+  File.symlink backup_file_full_path, dot_config_file_full_path
+end
+
