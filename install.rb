b4326761a63ffe28a4df90be6324ad801ea8d556
Benny Wong <bdotdub@gmail.com>
2010-05-01 15:24:13 -0400

    Detecting whether the files are installed before linking

--- install.rb
+++ install.rb
@@ -8,17 +8,21 @@ config_backup = File.join home_dir, '.dotfiles'
 # Move into this dir
 Dir.chdir repo_dir
 
-ignore= ['install.rb', 'uninstall.rb', 'README.md', '.git', '.gitignore']
+ignore = ['install.rb', 'uninstall.rb', 'README.md', '.git', '.gitignore', '.gitignore.sample']
 Dir.glob('*').reject{|file| ignore.include?(file) }.each do |config_file|
   dot_config_file           = ".#{config_file}"
   dot_config_file_full_path = File.join home_dir, dot_config_file
   config_file_full_path     = File.join repo_dir, config_file
 
-  if File.exists?(File.join(home_dir, dot_config_file))
-    Dir.mkdir config_backup unless File.exists?(config_backup)
-
-    puts "Moving file from #{dot_config_file_full_path} to #{File.join(config_backup, config_file)}"
-    File.move dot_config_file_full_path, File.join(config_backup, dot_config_file)
+  if File.exists?(dot_config_file_full_path)
+    if File.symlink?(dot_config_file_full_path) and File.identical?(config_file_full_path, dot_config_file_full_path)
+      puts "#{config_file} already installed at #{config_file_full_path}"
+      next
+    else
+      Dir.mkdir config_backup unless File.exists?(config_backup)
+      puts "Moving file from #{dot_config_file_full_path} to #{File.join(config_backup, config_file)}"
+      File.move dot_config_file_full_path, File.join(config_backup, dot_config_file)
+    end
   end
 
   puts "Linking file #{config_file_full_path} to #{dot_config_file_full_path}"
