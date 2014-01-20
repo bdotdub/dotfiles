93fc431372e827726de74fb219ba995940ab7dfc
Benny Wong <benny@kungfury.local>
2014-01-20 17:51:01 -0500

    Add xcode and osx things

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -1,4 +1,10 @@
-# via https://github.com/mathiasbynens/dotfiles
+# via https://github.com/mathiasbynens/dotfiles and https://github.com/holman/dotfiles
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Enable the 2D Dock
 defaults write com.apple.dock no-glass -bool true
@@ -20,6 +26,9 @@ defaults write com.apple.dock mouse-over-hilte-stack -bool true
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
 
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
@@ -66,6 +75,31 @@ chflags nohidden ~/Library
 
 # Disable local Time Machine backups
 sudo tmutil disablelocal
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Desktop
+defaults write com.apple.dock wvous-tl-corner -int 4
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right screen corner → Show Application Windows
+defaults write com.apple.dock wvous-tr-corner -int 3
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start Mission Control
+defaults write com.apple.dock wvous-bl-corner -int 2
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right screen corner → Start screen saver
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 # Kill affected applications
 for app in Safari Finder Dock Mail; do killall "$app"; done
